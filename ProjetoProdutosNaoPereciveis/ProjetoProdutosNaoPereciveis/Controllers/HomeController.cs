fafd2f86ed8a1720353b0e289aa6b88a7c8ea753
HenriqueRoncon <henrique.pereira77@etec.sp.gov.br>
2021-06-17 20:07:28 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoProdutosNaoPereciveis/ProjetoProdutosNaoPereciveis/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoProdutosNaoPereciveis.Models;
+
+namespace ProjetoProdutosNaoPereciveis.Controllers
+{
+    public class HomeController : Controller
+    {
+            public ActionResult Index()
+            {
+                var naoPerecivel = new ProdutosNaoPereciveis();
+                return View(naoPerecivel);
+            }
+            [HttpPost]
+            public ActionResult Index(ProdutosNaoPereciveis naoPerecivel)
+            {
+                if (ModelState.IsValid)
+                {
+                    return View("Resultado", naoPerecivel);
+                }
+                return View(naoPerecivel);
+            }
+            public ActionResult Resultado(ProdutosNaoPereciveis naoPerecivel)
+            {
+                return View(naoPerecivel);
+            }
+
+            public ActionResult About()
+            {
+                ViewBag.Message = "Your application description page.";
+
+                return View();
+            }
+
+            public ActionResult Contact()
+            {
+                ViewBag.Message = "Your contact page.";
+
+                return View();
+            }
+        
+    }
+}
---
+++ ProjetoProdutosNaoPereciveis/ProjetoProdutosNaoPereciveis/Models/ProdutosNaoPereciveis.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace ProjetoProdutosNaoPereciveis.Models
+{
+    public class ProdutosNaoPereciveis
+    {
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Insira de 2 a 30 caracteres.")]
+        public string Produtos { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [StringLength(150, MinimumLength = 5, ErrorMessage = "Insira de 5 a 150 caracteres.")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(1, 80, ErrorMessage = "Insira um peso, em kg, de 1 a 80")]
+        public int Peso { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Insira de 5 a 100 caracteres")]
+        public string Categoria { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(1, 750, ErrorMessage = "Insira um valor de 1 a 750R$")]
+        public int Valor { get; set; }
+    }
+}
